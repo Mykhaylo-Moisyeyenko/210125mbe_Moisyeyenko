72e66a929c17781f88ca5448a770a9ccc2a65395
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-03-26 22:51:09 +0100

    ДЗ Моисеенко

---
+++ src/main/java/de/telran/DZ_nio_20250325/Task1/MainTask1.java
@@ -0,0 +1,39 @@
+package de.telran.DZ_nio_20250325.Task1;
+
+//Написать приложение, которое будет копировать информацию из файла в другой файл
+//с использованием NIO.
+//Путь к существующему файлу и имя нового файла нужно вводить с клавиатуры.
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class MainTask1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите полный путь (с именем файла) к исходному файлу для копирования: ");
+        String file1 = scanner.nextLine();
+
+        System.out.println("Введите полный путь (с именем) для нового файла: ");
+        String file2 = scanner.nextLine();
+
+        Path sourceFilePath = Path.of(file1);
+        Path targetFilePath = Path.of(file2);
+
+        String string;
+        try (BufferedReader reader = Files.newBufferedReader(sourceFilePath);
+                BufferedWriter writer = Files.newBufferedWriter(targetFilePath)) {
+            while ((string = reader.readLine()) != null) {
+                writer.write(string);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/de/telran/DZ_nio_20250325/Task2/Client.java
@@ -0,0 +1,93 @@
+package de.telran.DZ_nio_20250325.Task2;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Client implements Serializable {
+
+    private Long id;
+    private String status;
+    private Long taxCode;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(Long taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Client(Long id, String status, Long taxCode, String firstName, String lastName, String email, String address, String phone) {
+        this.id = id;
+        this.status = status;
+        this.taxCode = taxCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+
+    }
+
+}
