00ea0c381da8f4b7b134b2b369fde53cb3bf2a7c
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-04-14 15:06:23 +0200

    ДЗ Моисеенко

---
+++ src/main/java/de/telran/lesson24/BookStorage.java
@@ -0,0 +1,17 @@
+package de.telran.lesson24;
+
+import java.util.HashMap;
+
+public class BookStorage {
+
+    //HashMap - key - isbn, value - Book
+
+    public void add(Book book) {
+        //
+
+    }
+
+    public Book get(String isbn) {
+        return null;
+    }
+}
